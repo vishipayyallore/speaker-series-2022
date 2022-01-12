8618792fd413a4dbaaaa90eef4a436e4c7a18db8
Viswanatha Swamy <vishipayyallore@yahoo.com>
2022-01-12 13:48:42 +0530

    Today's work

---
+++ microsoft-reactor/S9_2022Jan12_WebAppsDeploymentSlots/Source/College.Web/WebJob1/Functions.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Azure.WebJobs;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebJob1
+{
+    public class Functions
+    {
+        // This function will get triggered/executed when a new message is written 
+        // on an Azure Queue called queue.
+        public static void ProcessQueueMessage([QueueTrigger("paymentsqueue")] string message, TextWriter log)
+        {
+            log.WriteLine(message);
+        }
+    }
+}
