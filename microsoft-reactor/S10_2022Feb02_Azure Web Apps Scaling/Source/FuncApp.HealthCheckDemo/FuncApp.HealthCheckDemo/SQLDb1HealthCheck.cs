cca655771058bfaae2dd2b6b73cb7515a4dad196
Viswanatha Swamy <vishipayyallore@yahoo.com>
2022-02-17 17:31:11 +0530

    Initial Version

--- microsoft-reactor/S10_2022Feb02_Azure Web Apps Scaling/Source/FuncApp.HealthCheckDemo/FuncApp.HealthCheckDemo/SQLDb1HealthCheck.cs
+++ microsoft-reactor/S10_2022Feb02_Azure Web Apps Scaling/Source/FuncApp.HealthCheckDemo/FuncApp.HealthCheckDemo/SQLDb1HealthCheck.cs
@@ -4,9 +4,7 @@ using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Extensions.Diagnostics.HealthChecks;
 using Microsoft.Extensions.Logging;
-using Newtonsoft.Json;
 using System;
-using System.IO;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -30,17 +28,8 @@ namespace FuncApp.HealthCheckDemo
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
 
-            string name = req.Query["name"];
-
-            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name ??= data?.name;
-
-            string responseMessage = string.IsNullOrEmpty(name)
-                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-                : $"Hello, {name}. This HTTP triggered function executed successfully.";
-
             var result = await _healthCheckService.CheckHealthAsync((check) => check.Tags.Contains("SQLDatabase1Check"), cancellationToken);
+
             return new OkObjectResult(result);
         }
 
