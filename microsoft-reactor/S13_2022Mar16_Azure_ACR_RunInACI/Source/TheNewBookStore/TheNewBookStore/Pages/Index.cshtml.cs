332e817442cf0e5937209de02626ddd7d4d947a0
Viswanatha Swamy P K <vishipayyallore@yahoo.com>
2022-03-16 10:53:54 +0530

    After the session

---
+++ microsoft-reactor/S13_2022Mar16_Azure_ACR_RunInACI/Source/TheNewBookStore/TheNewBookStore/Pages/Index.cshtml.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace TheNewBookStore.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        private readonly IConfiguration _configuration;
+
+        public string ConnectionString { get; set; }
+
+        public IndexModel(ILogger<IndexModel> logger, IConfiguration configuration)
+        {
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+
+            _configuration = configuration ?? throw new ArgumentNullException(nameof(configuration));
+        }
+
+        public void OnGet()
+        {
+            ConnectionString = _configuration["MongoDbSettings:ConnectionString"];
+        }
+    }
+}
