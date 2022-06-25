76c77b992db72c60f5e5946cbcaf55e17cf84f2a
Viswanatha Swamy <vishipayyallore@yahoo.com>
2022-06-26 00:44:23 +0530

    UPload the file

--- aws-learning/S4_26Jun_.NET6_S3/Source/AWSS3.BasicsDemo/AWSS3.BasicsDemo/Program.cs
+++ aws-learning/S4_26Jun_.NET6_S3/Source/AWSS3.BasicsDemo/AWSS3.BasicsDemo/Program.cs
@@ -19,9 +19,14 @@ string _keyName = _configuration["AWSS3:KeyName"];
 
 using (IAmazonS3 client = new AmazonS3Client())
 {
-    await CreateBucketAsync(_bucketName, client);
-}
 
+    Console.ResetColor();
+    await CreateBucketAsync(_bucketName, client);
+    Console.ResetColor();
+
+    await UploadFileAsync(_bucketName, _filePath, client);
+    Console.ResetColor();
+}
 
 WriteLine("\n\nPress any key ...");
 
@@ -30,10 +35,30 @@ static async Task CreateBucketAsync(string _bucketName, IAmazonS3 client)
     var success = await S3BucketHelper.CreateBucketAsync(client, _bucketName);
     if (success)
     {
+        Console.ForegroundColor = ConsoleColor.Green;
         WriteLine($"Successfully created bucket: {_bucketName}.\n");
     }
     else
     {
+        Console.ForegroundColor = ConsoleColor.Red;
         WriteLine($"Could not create bucket: {_bucketName}.\n");
+    }
+}
+
+static async Task UploadFileAsync(string _bucketName, string _filePath, IAmazonS3 client)
+{
+    string _keyName = Path.GetFileName(_filePath);
+
+    var success = await S3BucketHelper.UploadFileAsync(client, _bucketName, _keyName, _filePath);
+
+    if (success)
+    {
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine($"Successfully uploaded {_keyName} from {_filePath} to {_bucketName}.\n");
+    }
+    else
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine($"Could not upload {_keyName}.\n");
     }
 }
--- aws-learning/S4_26Jun_.NET6_S3/Source/AWSS3.BasicsDemo/AWSS3.BasicsDemo/S3BucketHelper.cs
+++ aws-learning/S4_26Jun_.NET6_S3/Source/AWSS3.BasicsDemo/AWSS3.BasicsDemo/S3BucketHelper.cs
@@ -31,6 +31,27 @@ namespace AWSS3.BasicsDemo
             }
         }
 
+        public static async Task<bool> UploadFileAsync(IAmazonS3 client, string bucketName, string objectName, string filePath)
+        {
+            var request = new PutObjectRequest
+            {
+                BucketName = bucketName,
+                Key = objectName,
+                FilePath = filePath,
+            };
+
+            var response = await client.PutObjectAsync(request);
+            if (response.HttpStatusCode == System.Net.HttpStatusCode.OK)
+            {
+                Console.WriteLine($"Successfully uploaded {objectName} to {bucketName}.");
+                return true;
+            }
+            else
+            {
+                Console.WriteLine($"Could not upload {objectName} to {bucketName}.");
+                return false;
+            }
+        }
     }
 
 }
