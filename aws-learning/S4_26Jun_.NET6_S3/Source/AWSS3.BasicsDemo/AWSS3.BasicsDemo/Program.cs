d195c6f178e1c53c38900ad262fc716edbf86467
Viswanatha Swamy <vishipayyallore@yahoo.com>
2022-06-26 01:13:49 +0530

    Verified all three actions

--- aws-learning/S4_26Jun_.NET6_S3/Source/AWSS3.BasicsDemo/AWSS3.BasicsDemo/Program.cs
+++ aws-learning/S4_26Jun_.NET6_S3/Source/AWSS3.BasicsDemo/AWSS3.BasicsDemo/Program.cs
@@ -16,71 +16,19 @@ string _bucketName = _configuration["AWSS3:BucketName"];
 string _filePath = _configuration["AWSS3:UploadsFilePath"];
 string _keyName = _configuration["AWSS3:KeyName"];
 
-
 using (IAmazonS3 client = new AmazonS3Client())
 {
-
     Console.ResetColor();
-    await CreateBucketAsync(_bucketName, client);
+    await S3BucketHelper.CreateBucketAsync(_bucketName, client);
     Console.ResetColor();
 
-    _keyName = await UploadFileAsync(_bucketName, _filePath, client);
+    _keyName = await S3BucketHelper.UploadFileAsync(_bucketName, _filePath, client);
     Console.ResetColor();
 
     _filePath = _configuration["AWSS3:DownFilePath"];
-    await DownloadObjectFromBucketAsync(_bucketName, _filePath, _keyName, client);
+    await S3BucketHelper.DownloadObjectFromBucketAsync(_bucketName, _filePath, _keyName, client);
     Console.ResetColor();
 }
 
 WriteLine("\n\nPress any key ...");
 
-static async Task CreateBucketAsync(string _bucketName, IAmazonS3 client)
-{
-    var success = await S3BucketUtility.CreateBucketAsync(client, _bucketName);
-    if (success)
-    {
-        Console.ForegroundColor = ConsoleColor.Green;
-        WriteLine($"Successfully created bucket: {_bucketName}.\n");
-    }
-    else
-    {
-        Console.ForegroundColor = ConsoleColor.Red;
-        WriteLine($"Could not create bucket: {_bucketName}.\n");
-    }
-}
-
-static async Task<string> UploadFileAsync(string _bucketName, string _filePath, IAmazonS3 client)
-{
-    string _keyName = Path.GetFileName(_filePath);
-
-    var success = await S3BucketUtility.UploadFileAsync(client, _bucketName, _keyName, _filePath);
-
-    if (success)
-    {
-        Console.ForegroundColor = ConsoleColor.Green;
-        WriteLine($"Successfully uploaded {_keyName} from {_filePath} to {_bucketName}.\n");
-    }
-    else
-    {
-        Console.ForegroundColor = ConsoleColor.Red;
-        WriteLine($"Could not upload {_keyName}.\n");
-    }
-
-    return _keyName;
-}
-
-static async Task DownloadObjectFromBucketAsync(string _bucketName, string _filePath, string _keyName, IAmazonS3 client)
-{
-    var success = await S3BucketUtility.DownloadObjectFromBucketAsync(client, _bucketName, _keyName, _filePath);
-
-    if (success)
-    {
-        Console.ForegroundColor = ConsoleColor.Green;
-        WriteLine($"Successfully downloaded {_keyName}.\n");
-    }
-    else
-    {
-        Console.ForegroundColor = ConsoleColor.Red;
-        WriteLine($"Sorry, could not download {_keyName}.\n");
-    }
-}
---
+++ aws-learning/S4_26Jun_.NET6_S3/Source/AWSS3.BasicsDemo/AWSS3.BasicsDemo/S3BucketHelper.cs
@@ -0,0 +1,66 @@
+﻿using Amazon.S3;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using static System.Console;
+
+namespace AWSS3.BasicsDemo
+{
+
+    internal class S3BucketHelper
+    {
+        public static async Task CreateBucketAsync(string _bucketName, IAmazonS3 client)
+        {
+            var success = await S3BucketUtility.CreateBucketAsync(client, _bucketName);
+            if (success)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                WriteLine($"Successfully created bucket: {_bucketName}.\n");
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                WriteLine($"Could not create bucket: {_bucketName}.\n");
+            }
+        }
+
+        public static async Task<string> UploadFileAsync(string _bucketName, string _filePath, IAmazonS3 client)
+        {
+            string _keyName = Path.GetFileName(_filePath);
+
+            var success = await S3BucketUtility.UploadFileAsync(client, _bucketName, _keyName, _filePath);
+
+            if (success)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                WriteLine($"Successfully uploaded {_keyName} from {_filePath} to {_bucketName}.\n");
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                WriteLine($"Could not upload {_keyName}.\n");
+            }
+
+            return _keyName;
+        }
+
+        public static async Task DownloadObjectFromBucketAsync(string _bucketName, string _filePath, string _keyName, IAmazonS3 client)
+        {
+            var success = await S3BucketUtility.DownloadObjectFromBucketAsync(client, _bucketName, _keyName, _filePath);
+
+            if (success)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                WriteLine($"Successfully downloaded {_keyName}.\n");
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                WriteLine($"Sorry, could not download {_keyName}.\n");
+            }
+        }
+    }
+}
