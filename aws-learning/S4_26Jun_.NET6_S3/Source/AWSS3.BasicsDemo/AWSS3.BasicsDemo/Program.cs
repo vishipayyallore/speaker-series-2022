f2de66438dff08354ccec90be251a00fb9089e17
Viswanatha Swamy <vishipayyallore@yahoo.com>
2022-06-26 00:29:11 +0530

    Bucket Creation

--- aws-learning/S4_26Jun_.NET6_S3/Source/AWSS3.BasicsDemo/AWSS3.BasicsDemo/Program.cs
+++ aws-learning/S4_26Jun_.NET6_S3/Source/AWSS3.BasicsDemo/AWSS3.BasicsDemo/Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+ * Reference: https://github.com/awsdocs/aws-doc-sdk-examples/tree/main/dotnetv3/S3/S3_Basics
+ */
+
+using Amazon.S3;
+using AWSS3.BasicsDemo;
+using Microsoft.Extensions.Configuration;
+using static System.Console;
+
+IConfiguration _configuration = new ConfigurationBuilder()
+    .AddJsonFile("appsettings.json")
+    .AddUserSecrets("42B3177E-1CA6-448F-8F9A-1294955F5337")
+    .Build();
+
+string _bucketName = _configuration["AWSS3:BucketName"];
+string _filePath = _configuration["AWSS3:FilePath"];
+string _keyName = _configuration["AWSS3:KeyName"];
+
+
+using (IAmazonS3 client = new AmazonS3Client())
+{
+    await CreateBucketAsync(_bucketName, client);
+}
+
+
+WriteLine("\n\nPress any key ...");
+
+static async Task CreateBucketAsync(string _bucketName, IAmazonS3 client)
+{
+    var success = await S3BucketHelper.CreateBucketAsync(client, _bucketName);
+    if (success)
+    {
+        WriteLine($"Successfully created bucket: {_bucketName}.\n");
+    }
+    else
+    {
+        WriteLine($"Could not create bucket: {_bucketName}.\n");
+    }
+}
---
+++ aws-learning/S4_26Jun_.NET6_S3/Source/AWSS3.BasicsDemo/AWSS3.BasicsDemo/S3BucketHelper.cs
@@ -0,0 +1,36 @@
+﻿using Amazon.S3;
+using Amazon.S3.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AWSS3.BasicsDemo
+{
+    
+    internal class S3BucketHelper
+    {
+        public static async Task<bool> CreateBucketAsync(IAmazonS3 client, string bucketName)
+        {
+            try
+            {
+                var request = new PutBucketRequest
+                {
+                    BucketName = bucketName,
+                    UseClientRegion = true,
+                };
+
+                var response = await client.PutBucketAsync(request);
+                return response.HttpStatusCode == System.Net.HttpStatusCode.OK;
+            }
+            catch (AmazonS3Exception ex)
+            {
+                Console.WriteLine($"Error creating bucket: '{ex.Message}'");
+                return false;
+            }
+        }
+
+    }
+
+}
