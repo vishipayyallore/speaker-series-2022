99f11dc91abe8426c4f6df89ea599bc7dc1afb27
Viswanatha Swamy <vishipayyallore@yahoo.com>
2022-07-05 21:25:40 +0530

    ListBlobAsync, DownloadBlobAsync

--- microsoft-reactor/S18_2022Jul06_AzureBlobStorage/Source/AzBlobStorage.GettingStarted/AzBlobStorage.GettingStarted/Program.cs
+++ microsoft-reactor/S18_2022Jul06_AzureBlobStorage/Source/AzBlobStorage.GettingStarted/AzBlobStorage.GettingStarted/Program.cs
@@ -18,6 +18,8 @@ var containerClient = await CreateContainerAsync(blobServiceClient, containerNam
 await UploadBlobAsync(containerClient, _configuration["AzStorage:TextFileLocalPath"]);
 await UploadBlobAsync(containerClient, _configuration["AzStorage:PngFileLocalPath"]);
 
+await ListBlobAsync(containerClient);
+
 Console.WriteLine("\n\nPress any key ...");
 
 static async Task<BlobContainerClient> CreateContainerAsync(BlobServiceClient blobServiceClient, string containerName)
@@ -36,8 +38,31 @@ static async Task UploadBlobAsync(BlobContainerClient containerClient, string lo
     // Set the blob's content type so that the browser knows to treat it as an image.
     var fileUploadResults = await blobClient.UploadAsync(File.OpenRead($"{localFilePath}"), true);
 
-    if(fileUploadResults.GetRawResponse().Status == 201)
+    if (fileUploadResults.GetRawResponse().Status == 201)
     {
         Console.WriteLine($"{fileName} uploaded to the {blobClient.Uri} location");
     }
+}
+
+static async Task ListBlobAsync(BlobContainerClient containerClient)
+{
+    Console.WriteLine("\n\nDisplaying the Blobs");
+
+    var counter = 1;
+    await foreach (var blob in containerClient.GetBlobsAsync())
+    {
+        Console.WriteLine("{2}- {0} (type: {1})", blob.Name, blob.GetType(), (counter++));
+
+        await DownloadBlobAsync(containerClient, blob);
+    }
+}
+
+static async Task DownloadBlobAsync(BlobContainerClient containerClient, BlobItem blob)
+{
+    Console.WriteLine("\tDownloading {0}", blob.Name);
+
+    BlobClient blobClient = containerClient.GetBlobClient(blob.Name);
+    await blobClient.DownloadToAsync(string.Format("./Downloads/CopyOf{0}", blob.Name));
+
+    Console.WriteLine("\tDownloaded {0}", blob.Name);
 }
