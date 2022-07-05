192dd2e3cf492c6f9e720bb370c9c52ba9fc325f
Viswanatha Swamy <vishipayyallore@yahoo.com>
2022-07-05 19:56:48 +0530

    UploadBlobAsync

--- microsoft-reactor/S18_2022Jul06_AzureBlobStorage/Source/AzBlobStorage.GettingStarted/AzBlobStorage.GettingStarted/Program.cs
+++ microsoft-reactor/S18_2022Jul06_AzureBlobStorage/Source/AzBlobStorage.GettingStarted/AzBlobStorage.GettingStarted/Program.cs
@@ -1,5 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
 using Microsoft.Extensions.Configuration;
 
 IConfiguration _configuration = new ConfigurationBuilder()
@@ -11,18 +12,32 @@ IConfiguration _configuration = new ConfigurationBuilder()
 string storageConnectionString = _configuration["AzStorage:BlobStorageConnectionString"];
 BlobServiceClient blobServiceClient = new(storageConnectionString);
 
-string containerName = "demoblob" + Guid.NewGuid().ToString("N");
+string containerName = _configuration["AzStorage:BlobContainerName"];
 var containerClient = await CreateContainerAsync(blobServiceClient, containerName);
+
+await UploadBlobAsync(containerClient, _configuration["AzStorage:TextFileLocalPath"]);
+await UploadBlobAsync(containerClient, _configuration["AzStorage:PngFileLocalPath"]);
 
 Console.WriteLine("\n\nPress any key ...");
 
 static async Task<BlobContainerClient> CreateContainerAsync(BlobServiceClient blobServiceClient, string containerName)
 {
-    BlobContainerClient containerClient = await blobServiceClient.CreateBlobContainerAsync(containerName);
-    Console.WriteLine($"A container named '{containerName}' has been created. \nTake a minute and verify in the portal. \nNext a file will be created and uploaded to the container.");
-
-    BlobContainerClient container = blobServiceClient.GetBlobContainerClient(containerName);
-    await container.CreateIfNotExistsAsync();
+    BlobContainerClient containerClient = blobServiceClient.GetBlobContainerClient(containerName);
+    await containerClient.CreateIfNotExistsAsync();
 
     return containerClient;
+}
+
+static async Task UploadBlobAsync(BlobContainerClient containerClient, string localFilePath)
+{
+    var fileName = Path.GetFileName(localFilePath);
+    BlobClient blobClient = containerClient.GetBlobClient(fileName);
+
+    // Set the blob's content type so that the browser knows to treat it as an image.
+    var fileUploadResults = await blobClient.UploadAsync(File.OpenRead($"{localFilePath}"), true);
+
+    if(fileUploadResults.GetRawResponse().Status == 201)
+    {
+        Console.WriteLine($"{fileName} uploaded to the {blobClient.Uri} location");
+    }
 }
