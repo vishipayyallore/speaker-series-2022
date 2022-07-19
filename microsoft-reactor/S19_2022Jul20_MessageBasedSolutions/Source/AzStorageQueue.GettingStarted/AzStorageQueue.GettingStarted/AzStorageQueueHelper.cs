bf32d143ac55d5b3f715b9f91199588c67800ca4
Viswanatha Swamy <vishipayyallore@yahoo.com>
2022-07-19 13:04:30 +0530

    Improvement - Pass 1

---
+++ microsoft-reactor/S19_2022Jul20_MessageBasedSolutions/Source/AzStorageQueue.GettingStarted/AzStorageQueue.GettingStarted/AzStorageQueueHelper.cs
@@ -0,0 +1,86 @@
+﻿using Azure.Storage.Queues;
+using Azure.Storage.Queues.Models;
+
+using static System.Console;
+
+namespace AzStorageQueue.GettingStarted
+{
+    internal class AzStorageQueueHelper
+    {
+
+        public static QueueClient CreateQueueClient(string connectionString, string queueName)
+        {
+            return new QueueClient(connectionString, queueName);
+        }
+
+        public static async Task<bool> CreateQueue(QueueClient queueClient)
+        {
+            try
+            {
+                // Create the queue
+                await queueClient.CreateIfNotExistsAsync();
+
+                if (queueClient.Exists())
+                {
+                    WriteLine($"Queue created: '{queueClient.Name}'");
+                    return true;
+                }
+                else
+                {
+                    WriteLine($"Make sure the Azurite storage emulator running and try again.");
+                    return false;
+                }
+            }
+            catch (Exception ex)
+            {
+                WriteLine($"Exception: {ex.Message}\n\n");
+                WriteLine($"Make sure the Azurite storage emulator running and try again.");
+                return false;
+            }
+        }
+
+        public static async Task InsertMessage(QueueClient queueClient, string queueName, string message)
+        {
+            // Create the queue if it doesn't already exist
+            queueClient.CreateIfNotExists();
+
+            if (queueClient.Exists())
+            {
+                // Send a message to the queue
+                SendReceipt sendReceipt = await queueClient.SendMessageAsync(message);
+                WriteLine($"Content of Send Receipt: {sendReceipt}");
+            }
+
+            WriteLine($"Inserted: {message}");
+        }
+
+        public static async Task PeekMessage(QueueClient queueClient, string queueName)
+        {
+            if (queueClient.Exists())
+            {
+                // Peek at the next message
+                PeekedMessage[] peekedMessage = await queueClient.PeekMessagesAsync();
+
+                // Display the message
+                WriteLine($"Peeked message: '{peekedMessage[0].Body}'");
+            }
+        }
+
+        public static async Task UpdateMessage(QueueClient queueClient, string queueName)
+        {
+            if (queueClient.Exists())
+            {
+                // Get the message from the queue
+                QueueMessage[] message = await queueClient.ReceiveMessagesAsync();
+
+                // Update the message contents
+                await queueClient.UpdateMessageAsync(message[0].MessageId,
+                        message[0].PopReceipt,
+                        $"Updated contents :: {message[0].Body}",
+                        TimeSpan.FromSeconds(60.0)  // Make it invisible for another 60 seconds
+                    );
+            }
+        }
+    }
+
+}
--- microsoft-reactor/S19_2022Jul20_MessageBasedSolutions/Source/AzStorageQueue.GettingStarted/AzStorageQueue.GettingStarted/Program.cs
+++ microsoft-reactor/S19_2022Jul20_MessageBasedSolutions/Source/AzStorageQueue.GettingStarted/AzStorageQueue.GettingStarted/Program.cs
@@ -1,6 +1,8 @@
-﻿using Azure.Storage.Queues;
-using Azure.Storage.Queues.Models;
+﻿using AzStorageQueue.GettingStarted;
+using Azure.Storage.Queues;
 using Microsoft.Extensions.Configuration;
+
+using static System.Console;
 
 IConfiguration _configuration = new ConfigurationBuilder()
     .AddJsonFile("appsettings.json")
@@ -11,84 +13,17 @@ IConfiguration _configuration = new ConfigurationBuilder()
 string connectionString = _configuration["AzStorage:ConnectionString"];
 string queueName = _configuration["AzStorage:QueueName"];
 
-// Instantiate a QueueClient which will be used to create and manipulate the queue
-QueueClient queueClient = new QueueClient(connectionString, queueName);
+QueueClient queueClient = AzStorageQueueHelper.CreateQueueClient(connectionString, queueName);
 
-await CreateQueue(queueClient);
+await AzStorageQueueHelper.CreateQueue(queueClient);
 
 string message = $"Simple message {DateTime.Now}";
-await InsertMessage(queueClient, queueName, message);
+await AzStorageQueueHelper.InsertMessage(queueClient, queueName, message);
 
-await PeekMessage(queueClient, queueName);
+await AzStorageQueueHelper.PeekMessage(queueClient, queueName);
 
-await UpdateMessage(queueClient, queueName);
+await AzStorageQueueHelper.UpdateMessage(queueClient, queueName);
 
-Console.WriteLine("\n\nPress any key ...");
+WriteLine("\n\nPress any key ...");
 
-static async Task<bool> CreateQueue(QueueClient queueClient)
-{
-    try
-    {
-        // Create the queue
-        await queueClient.CreateIfNotExistsAsync();
 
-        if (queueClient.Exists())
-        {
-            Console.WriteLine($"Queue created: '{queueClient.Name}'");
-            return true;
-        }
-        else
-        {
-            Console.WriteLine($"Make sure the Azurite storage emulator running and try again.");
-            return false;
-        }
-    }
-    catch (Exception ex)
-    {
-        Console.WriteLine($"Exception: {ex.Message}\n\n");
-        Console.WriteLine($"Make sure the Azurite storage emulator running and try again.");
-        return false;
-    }
-}
-
-static async Task InsertMessage(QueueClient queueClient, string queueName, string message)
-{
-    // Create the queue if it doesn't already exist
-    queueClient.CreateIfNotExists();
-
-    if (queueClient.Exists())
-    {
-        // Send a message to the queue
-        await queueClient.SendMessageAsync(message);
-    }
-
-    Console.WriteLine($"Inserted: {message}");
-}
-
-static async Task PeekMessage(QueueClient queueClient, string queueName)
-{
-    if (queueClient.Exists())
-    {
-        // Peek at the next message
-        PeekedMessage[] peekedMessage = await queueClient.PeekMessagesAsync();
-
-        // Display the message
-        Console.WriteLine($"Peeked message: '{peekedMessage[0].Body}'");
-    }
-}
-
-static async Task UpdateMessage(QueueClient queueClient, string queueName)
-{
-    if (queueClient.Exists())
-    {
-        // Get the message from the queue
-        QueueMessage[] message = await queueClient.ReceiveMessagesAsync();
-
-        // Update the message contents
-        await queueClient.UpdateMessageAsync(message[0].MessageId,
-                message[0].PopReceipt,
-                $"Updated contents :: {message[0].Body}",
-                TimeSpan.FromSeconds(60.0)  // Make it invisible for another 60 seconds
-            );
-    }
-}
