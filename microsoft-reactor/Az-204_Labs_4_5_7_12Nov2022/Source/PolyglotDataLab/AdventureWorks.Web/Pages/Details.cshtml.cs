fc0f5ec7881039cf1aeb9ef16bf235169395b4fd
Viswanatha Swamy <vishipayyallore@yahoo.com>
2022-11-12 01:17:32 +0530

    Layout

--- microsoft-reactor/Az-204_Labs_4_5_7_12Nov2022/Source/PolyglotDataLab/AdventureWorks.Web/Pages/Details.cshtml.cs
+++ microsoft-reactor/Az-204_Labs_4_5_7_12Nov2022/Source/PolyglotDataLab/AdventureWorks.Web/Pages/Details.cshtml.cs
@@ -1,12 +1,30 @@
+using AdventureWorks.Context;
+using AdventureWorks.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel.DataAnnotations;
 
 namespace AdventureWorks.Web.Pages
 {
     public class DetailsModel : PageModel
     {
-        public void OnGet()
+        private readonly IAdventureWorksProductContext _productContext;
+
+        public DetailsModel(IAdventureWorksProductContext productContext)
         {
+            _productContext = productContext;
         }
+
+        [BindProperty(SupportsGet = true)]
+        public Model Model { get; set; } = new();
+
+        [BindProperty, Required(ErrorMessage = "Please select a product.")]
+        public string SelectedProductId { get; set; } = String.Empty;
+
+        public async Task OnGetAsync(Guid id)
+        {
+            this.Model = await _productContext.FindModelAsync(id);
+        }
+
     }
 }
