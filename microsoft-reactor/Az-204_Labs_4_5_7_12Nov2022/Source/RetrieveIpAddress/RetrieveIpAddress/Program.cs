dd0a3cf15f0168e92cc235246b3a09fa3e97d532
Viswanatha Swamy <vishipayyallore@yahoo.com>
2022-11-12 09:57:13 +0530

    Swamy/az 204 labs 12nov2022 (#39)
    * Initial version
    
    * Update README.md
    
    * Uploaded the Files
    
    * Sample
    
    * Few Changes
    
    * Model Class Library
    
    * Class Libraries
    
    * Index page
    
    * Layout
    
    * New Program

---
+++ microsoft-reactor/Az-204_Labs_4_5_7_12Nov2022/Source/RetrieveIpAddress/RetrieveIpAddress/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Net;
+using System.Net.NetworkInformation;
+using static System.Console;
+
+// Check if network is available
+if (NetworkInterface.GetIsNetworkAvailable())
+{
+    WriteLine("Current IP Addresses:");
+
+    // Get host entry for current hostname
+    string hostname = Dns.GetHostName();
+    IPHostEntry host = Dns.GetHostEntry(hostname);
+
+    // Iterate over each IP address and render their values
+    foreach (IPAddress address in host.AddressList)
+    {
+        WriteLine($"\t{address}");
+    }
+}
+else
+{
+    WriteLine("No Network Connection");
+}
