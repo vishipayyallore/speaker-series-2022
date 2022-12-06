1f04d9d1a3821660258ec3d5c6e3228399fb6ec5
Viswanatha Swamy <vishipayyallore@yahoo.com>
2022-12-07 00:25:17 +0530

    Core

---
+++ microsoft-reactor/S24_2022Dec07_EventBased_EventHubs/Source/Hospital.Core/Device.cs
@@ -0,0 +1,15 @@
+﻿namespace Hospital.Core
+{
+
+    public class Device
+    {
+        public string deviceId { get; set; } = "D101";
+
+        public string patientName { get; set; } = "No Name";
+
+        public string heartBeat { get; set; } = "72";
+
+        public string bloodPresure { get; set; } = "120/80";
+    }
+
+}
