94dc3a9ad1ea8f0f1cc778c040d44b1e2089048d
Viswanatha Swamy <vishipayyallore@yahoo.com>
2022-12-07 00:45:19 +0530

    Changes

---
+++ microsoft-reactor/S24_2022Dec07_EventBased_EventHubs/Source/Patient-SendData/Program.cs
@@ -0,0 +1,20 @@
+﻿using Azure.Messaging.EventHubs;
+using Azure.Messaging.EventHubs.Producer;
+using Microsoft.Extensions.Configuration;
+
+IConfiguration _configuration = new ConfigurationBuilder()
+    .AddUserSecrets("dc15d825-8181-4631-9245-31ec411f4dc3")
+    .Build();
+
+var connectionString = _configuration["ConnectionString"];
+var eventHubName = _configuration["EventHubName"];
+
+await using (var producer = new EventHubProducerClient(connectionString, eventHubName))
+{
+    using EventDataBatch eventBatch = await producer.CreateBatchAsync();
+
+    eventBatch.TryAdd(new EventData(new BinaryData("First")));
+    eventBatch.TryAdd(new EventData(new BinaryData("Second")));
+
+    await producer.SendAsync(eventBatch);
+}
