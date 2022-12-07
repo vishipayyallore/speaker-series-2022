94a727397376fac6af5b5fa84b2e8072efb1ce1e
Viswanatha Swamy <vishipayyallore@yahoo.com>
2022-12-07 08:24:19 +0530

    Swamy/06dec2022 az204 eventhubs session (#41)
    * INitial Version
    
    * Removed Images
    
    * Removed the code
    
    * Create Az204-Event-Hubs-Demos.sln
    
    * Update Az204-Event-Hubs-Demos.sln
    
    * Core
    
    * Changes
    
    * sending data
    
    * Sending Details
    
    * Send and Received Data

---
+++ microsoft-reactor/S24_2022Dec07_EventBased_EventHubs/Source/Patient-ReceiveData/Program.cs
@@ -0,0 +1,49 @@
+﻿using Azure.Messaging.EventHubs;
+using Azure.Messaging.EventHubs.Consumer;
+using Azure.Messaging.EventHubs.Producer;
+using Hospital.Core;
+using Microsoft.Azure.Amqp;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using System.Diagnostics;
+using System.Text;
+
+IConfiguration _configuration = new ConfigurationBuilder()
+    .AddUserSecrets("dc15d825-8181-4631-9245-31ec411f4dc3")
+    .Build();
+
+var connectionString = _configuration["ConnectionString"];
+var eventHubName = _configuration["EventHubName"];
+var consumerGroup = EventHubConsumerClient.DefaultConsumerGroupName;
+
+await GetPartitionIds();
+await ReadEvents();
+
+async Task GetPartitionIds()
+{
+    await using EventHubConsumerClient eventHubConsumerClient = new(consumerGroup, connectionString);
+
+    string[] partitionIds = await eventHubConsumerClient.GetPartitionIdsAsync();
+
+    foreach (string partitionId in partitionIds)
+    {
+        Console.WriteLine("Partition Id {0}", partitionId);
+    }
+}
+
+async Task ReadEvents()
+{
+    await using var consumer = new EventHubConsumerClient(consumerGroup, connectionString, eventHubName);
+
+    using CancellationTokenSource cancellationSource = new();
+    cancellationSource.CancelAfter(TimeSpan.FromSeconds(145));
+
+    await foreach (PartitionEvent _event in consumer.ReadEventsAsync(cancellationSource.Token))
+    {
+        Console.WriteLine($"Partition ID {_event.Partition.PartitionId}");
+        Console.WriteLine($"Data Offset {_event.Data.Offset}");
+        Console.WriteLine($"Sequence Number {_event.Data.SequenceNumber}");
+        Console.WriteLine($"Partition Key {_event.Data.PartitionKey}");
+        Console.WriteLine(Encoding.UTF8.GetString(_event.Data.EventBody));
+    }
+}
