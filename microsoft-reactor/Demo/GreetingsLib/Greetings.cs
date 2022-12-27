8156e569a398450eb109c11c4085f59f202675c7
Viswanatha Swamy <vishipayyallore@yahoo.com>
2022-12-27 16:02:15 +0530

    Included the MIP

--- microsoft-reactor/Demo/GreetingsLib/Greetings.cs
+++ microsoft-reactor/Demo/GreetingsLib/Greetings.cs
@@ -1,10 +1,19 @@
-﻿namespace GreetingsLib
+﻿using Microsoft.InformationProtection;
+
+namespace GreetingsLib
 {
     public class Greetings
     {
+
+        private ApplicationInfo _appInfo = new ApplicationInfo()
+        {
+            ApplicationName = "Application Asif Hussain"
+        };
+
         public string SayHello(string userName)
         {
-            return $"Hello, {userName}";
+
+            return $"Hello, {userName}. {_appInfo.ApplicationName}";
         }
     }
 }
