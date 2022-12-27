355aed9be693ebf4f08cc3b26f9d13347d7274b2
Viswanatha Swamy <vishipayyallore@yahoo.com>
2022-12-27 12:28:53 +0530

    Changes

---
+++ microsoft-reactor/Demo/UseGreetingsLibFW/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GreetingsLib;
+
+namespace UseGreetingsLibFW
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Greetings greetings = new Greetings();
+            var user = "Asif";
+
+           Console.WriteLine($"Greetings: {greetings.SayHello(user)}");
+            Console.ReadLine();
+        }
+    }
+}
